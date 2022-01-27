2bda83dc4d8a56ed5db92a29107c6b66b9bb75fb
Eric Dietrich <eric.dietrich@alpes.one>
2022-01-27 19:01:35 -0300

    creating room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
+import { FormEvent, useState } from 'react';
 import {useHistory} from 'react-router-dom';
 
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
  
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg'
@@ -9,10 +11,10 @@ import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
 
-
 export const Home = () => {
-  const {user, signInWithGoogle} = useAuth()
+  const [roomCode, setRoomCode] = useState('')
 
+  const {user, signInWithGoogle} = useAuth()
   const history = useHistory();
 
 
@@ -22,6 +24,23 @@ export const Home = () => {
     }
     history.push('/rooms/new')
     
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if(roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+    
+    if(!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`rooms/${roomCode}`)
   }
 
   return (
@@ -39,10 +58,12 @@ export const Home = () => {
             Crie sua sala com o Google
           </button>
           <div className='separator'>ou entre em uma sala</div>
-          <form action="">
+          <form  onSubmit={handleJoinRoom}>
             <input  
               type="text" 
               placeholder="Digite o código da sala"
+              value={roomCode}
+              onChange={(event)=> setRoomCode(event.target.value)}
             />
             <Button type="submit">
               Entrar na sala
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,45 @@
-export function Room() {
+// import { database } from '../services/firebase';
+
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+
+ export function Room() {
+/*   const roomRef = database.ref('rooms')
+
+  async function getRoomData() {
+    const firebaseRoom = await roomRef.get();
+    console.log(firebaseRoom);
+
+  }
+
+  getRoomData(); */
+
   return (
-    <h1>Sala</h1>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="letmeask" />
+          <div>código</div>
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+      </main>
+
+      <form>
+        <textarea
+          placeholder='O que você quer perguntar?'
+        />
+        <div className="form-footer">
+          <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+          <Button type='submit'>Enviar pergunta</Button>
+        </div>
+      </form>
+
+    </div>
   )
 }
