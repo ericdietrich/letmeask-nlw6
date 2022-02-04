ab7a7f0249ffcbeff9c0807c6dabba5bd58217b7
Eric Dietrich <eric.dietrich@alpes.one>
2022-02-04 16:10:19 -0300

    adminRoom structure

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -13,7 +13,7 @@ type RoomParams = {
   id: string
 }
 
- export function Room() {
+ export function AdminRoom() {
   const {user} = useAuth();
   const params = useParams<RoomParams>();
   const roomId = params.id;
@@ -50,7 +50,10 @@ type RoomParams = {
       <header>
         <div className="content">
           <img src={logoImg} alt="letmeask" />
-          <RoomCode code={roomId} />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined >Encerrar sala</Button>
+          </div>
         </div>
       </header> 
 
@@ -59,26 +62,7 @@ type RoomParams = {
           <h1>Sala {title}</h1>
           {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
-    
-        <form onSubmit={handleSendQuestion}>
-          <textarea
-            placeholder='O que você quer perguntar?'
-            onChange={(event) => setNewQuestion(event.target.value)}
-            value={newQuestion}
-          />
-          <div className="form-footer">
-            { user ? (
-              <div className='user-info'>
-                <img src={user.avatar} alt={user.name} />
-                <span>{user.name}</span>
-              </div>
-            ) : (
-              <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-            )}
-            <Button type='submit' disabled={!user}>Enviar pergunta</Button>
-          </div>
-        </form>
-        
+      
         <div className="question-list">
           {questions && questions.map(question => {
             return (
